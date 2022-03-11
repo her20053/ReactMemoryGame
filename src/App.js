cfd34e080448fa6e908dd0f4fa026d34e27a39fb
Joe <her20053@uvg.edu.gt>
2022-03-11 11:38:11 -0600

    Agregadas Cartas y efectos
    Se agregaron las cartas aleatorizadas y effectos en el texto del Lab

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
+
+// Creamos una variable que mantenga un link src hacia las 6 posibles imagenes:
+const imagenesEnCartas = [
+    {"src": "/imagenes/darth-vader.png"},
+    {"src": "/imagenes/death-star.png"},
+    {"src": "/imagenes/jedi-order.png"},
+    {"src": "/imagenes/light-saber.png"},
+    {"src": "/imagenes/millennium-falcon.png"},
+    {"src": "/imagenes/r2d2.png"}
+]
+
 function App(){
 
+    // Creamos un estado en el que 'cartas' es nuestra variable y 'establecerCartas' es nuestro metodo
+    // Lo instanciamos a que empiece con un array vacio:
+    const [cartas, establecerCartas] = React.useState([])
+
+    // Creamos un estado en el que 'turnos' es nuestra variable y 'establecerTurnos' es nuestro metodo
+    // Lo instanciamos a que empiece con 0 turnos usados
+    const [turnos, establecerTurnos] = React.useState(0)
+
+    // Aleatorizamos las cartas:
+    const aleatorizarCartas = () =>{
+        // Creamos una array que tenga descompuestos 2 veces el array 'imagenesEnCartas' para tener un total de 12 cartas
+        // con cada imagen teniendo su pareja:
+        const cartasAleatorizadas = [...imagenesEnCartas, ...imagenesEnCartas]
+        // Con el metodo sort, aleatorizamos las cartas:
+        .sort(() => Math.random() - 0.5 ) /*Creamos una arrow function que si el numero es positivo cambiara de posicion la carta*/
+        // Para la nueva array ya aleatorizada por cada elemento aplicamos un id:
+        .map((carta) => ({ ...carta, id: Math.random()})) /*Descomponemos sus atributos que solo son src y le agregamos un rnd id */
+    
+    // Utilizamos el metodo del estado creado previamente y le enviamos como parametro las cartas aleatorias:
+    establecerCartas(cartasAleatorizadas)
+    // Establecemos el valor de turnos a 0 al crear un nuevo juego presionando el boton:
+    establecerTurnos(0)
+    }
+
+    // Le creamos un estilo a la APP:
     const style_app = {
         maxWidth: '860px',
         margin: '40px auto'
     }
 
+    console.log(cartas,turnos)
+
     return(
         <div style={style_app}>
             <h1>Laboratorio React JS</h1>
-            <button>Nuevo Juego</button>
+            <button onClick = {aleatorizarCartas}>Nuevo Juego</button>
         </div>  
     )
 }
